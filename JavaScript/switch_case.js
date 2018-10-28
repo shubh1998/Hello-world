94739e19c7081b462a1e61f7f6aa00fb859eadda
Shubham Gupta <shubhamgupta7440@gmail.com>
2018-10-28 19:45:05 +0530

    Create switch_case.js

---
+++ JavaScript/switch_case.js
@@ -0,0 +1,18 @@
+var ch=2;
+switch(ch)
+{
+  case 1:
+    alert("hello world!");
+    break;
+    
+  case 2:
+    alert("hello hacktoberfest, its time of contribution.");
+    break;
+    
+  case 3:
+    document.write("<h1> Yeah thats good </h2>");
+    break;
+    
+  default:
+    alert("Wrong Choice");
+}
